cd9dedb88dd130070b5be85f7269f16ba4327f45
Marco Descher <marco@descher.at>
2013-07-02 08:56:09 +0200

    Bug 62575 sample application

---
+++ at.descher.bug62575/src/at/descher/bug62575/parts/SamplePart.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package at.descher.bug62575.parts;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.workbench.swt.modeling.EMenuService;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class SamplePart {
+
+	@Inject private EMenuService menuService;
+	
+	private Label label;
+	private TableViewer tableViewer;
+	
+	private static final String ID_PART_POPUP_MENU = "samplePart.popupmenu";
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+		parent.setLayout(new GridLayout());
+
+		label = new Label(parent, SWT.NONE);
+		label.setText("Sample table");
+
+		tableViewer = new TableViewer(parent);
+		tableViewer.add("Sample item 1");
+		tableViewer.add("Sample item 2");
+		tableViewer.add("Sample item 3");
+		tableViewer.add("Sample item 4");
+		tableViewer.add("Sample item 5");
+		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		menuService.registerContextMenu(tableViewer.getTable(), ID_PART_POPUP_MENU);
+		
+	}
+
+	@Focus
+	public void setFocus() {
+		tableViewer.getTable().setFocus();
+	}
+}
